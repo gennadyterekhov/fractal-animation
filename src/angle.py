af9989b7d4e39e158894ee00efd274aeb33e6ca2
Gennady Terekhov <ter.ge@yandex.ru>
2026-02-15 18:57:09 +0300

    upd

--- src/angle.py
+++ src/angle.py
@@ -2,32 +2,11 @@ import math
 
 
 def get_angle(iter: int, rays: int) -> float:
-    print()
-    part = 360/rays
-    print('360/rays', part)
-    print('part*iter', part*iter)
-    angle = 180-part*iter
+    one_ray_degree = 360/rays
+    print('one_ray_degree*iter', one_ray_degree*iter)
+    angle = 180-one_ray_degree*iter
     return angle
 
 
 def to_rads(angle_in_degrees: float) -> float:
     return math.pi/180 * angle_in_degrees
-
-
-def main():
-    ang = get_angle(0, 4)
-    assert (ang == 180)
-
-    ang = get_angle(1, 4)
-    print('ang',ang)
-    assert (ang == 90)
-
-    ang = get_angle(2, 4)
-    assert (ang == 0)
-
-    ang = get_angle(3, 4)
-    assert (ang == -90)
-
-
-if __name__ == '__main__':
-    main()
--- tests/angle.py
+++ tests/angle.py
@@ -1,11 +1,12 @@
-from src.angle import get_angle
+from src.angle import get_angle, to_rads
+import math
 
-def main():
+
+def test_angle():
     ang = get_angle(0, 4)
     assert (ang == 180)
 
     ang = get_angle(1, 4)
-    print('ang',ang)
     assert (ang == 90)
 
     ang = get_angle(2, 4)
@@ -13,6 +14,57 @@ def main():
 
     ang = get_angle(3, 4)
     assert (ang == -90)
+
+    ang = get_angle(0, 5)
+    assert (ang == 180)
+
+    ang = get_angle(1, 5)
+    print('ang', ang)
+    assert (ang == 108)
+
+    ang = get_angle(2, 5)
+    assert (ang == 36)
+
+    ang = get_angle(3, 5)
+    assert (ang == -36)
+
+    ang = get_angle(4, 5)
+    assert (ang == -108)
+
+
+def test_rads():
+    rads = to_rads(0)
+    assert (assert_floats(rads, 0))
+
+    rads = to_rads(30)
+    assert (assert_floats(rads, 0.523))
+
+    rads = to_rads(45)
+    assert (assert_floats(rads, 0.785))
+
+    rads = to_rads(60)
+    assert (assert_floats(rads, 1.047))
+
+    rads = to_rads(180)
+    print('rads', rads)
+    assert (assert_floats(rads, 3.141))
+
+
+def assert_floats(a, b) -> bool:
+    epsilon = 0.001
+
+    aok = in_region(a, b-epsilon, b+epsilon)
+    bok = in_region(b, a-epsilon, a+epsilon)
+    return aok and bok
+
+
+def in_region(point, a, b) -> bool:
+    return point >= a and point <= b
+
+
+def main():
+    test_angle()
+    test_rads()
 
 
 if __name__ == '__main__':
