7f9fa510d6b68ee2210350257d5d27e346a2df49
Gennady Terekhov <ter.ge@yandex.ru>
2026-02-15 16:44:51 +0300

    upd

--- main.py
+++ main.py
@@ -3,22 +3,29 @@ from manim import *
 
 
 class StartingFromTheLeftCircle(Scene):
-    def make_1_iteration_and_return_starting_circle(self, starting_circle):
+    def make_1_iteration_and_return_starting_circle_and_all_prev_objs(self, starting_circle, all_objects: VGroup) -> dict:
+        all_circles = VGroup()
+        all_lines = VGroup()
+
         central_circle = Circle(color=WHITE)
         central_circle.next_to(
             starting_circle, RIGHT, buff=0.5)  # set the position
         self.play(Create(central_circle))
-        # self.add_the_rest
 
-        # add connecting line
         connecting_line = self.make_connecting_line(
-            starting_circle, central_circle)
+            starting_circle,
+            central_circle,
+        )
 
         self.play(Create(connecting_line))
 
         up_circle = self.make_filled_circle()
         down_circle = self.make_filled_circle()
         right_circle = self.make_filled_circle()
+
+        all_circles.add(up_circle, down_circle, right_circle,
+                        starting_circle, central_circle)
+        all_objects.add(*all_circles)
 
         up_circle.next_to(central_circle, UP, buff=0.5)
         down_circle.next_to(central_circle, DOWN, buff=0.5)
@@ -30,49 +37,71 @@ class StartingFromTheLeftCircle(Scene):
         connecting_line4 = self.make_connecting_line(
             right_circle, central_circle)
 
-        self.play(Create(up_circle), Create(down_circle), Create(right_circle), Create(
-            connecting_line2), Create(connecting_line3), Create(connecting_line4))
+        all_lines.add(connecting_line, connecting_line2,
+                      connecting_line3, connecting_line4)
+        all_objects.add(*all_lines)
 
-        all_objects = VGroup(up_circle, down_circle, right_circle, central_circle, starting_circle,
-                             connecting_line, connecting_line2, connecting_line3, connecting_line4)
+        self.play(
+            Create(up_circle),
+            Create(down_circle),
+            Create(right_circle),
+            Create(connecting_line2),
+            Create(connecting_line3),
+            Create(connecting_line4),
+        )
+
+        # all_objects = VGroup(up_circle, down_circle, right_circle, central_circle, starting_circle,
+        #  connecting_line, connecting_line2, connecting_line3, connecting_line4)
         self.play(
             all_objects.animate.shift(LEFT * 2.5),
             run_time=2
         )
 
-        next_level_left = Circle(color=WHITE,
-                                 radius=all_objects.width/2 + 0.5  # Add padding
-                                 )
+        next_level_left = Circle(
+            color=WHITE,
+            radius=all_objects.width/2 + 0.5  # Add padding
+        )
         next_level_left.move_to(all_objects.get_center())
         self.play(Create(next_level_left))
 
+        all_objects.add(next_level_left)
         all_plus_next_level_left = VGroup(up_circle, down_circle, right_circle, central_circle, starting_circle,
                                           connecting_line, connecting_line2, connecting_line3, connecting_line4, next_level_left)
         self.wait(0.1)
         self.play(
-            all_plus_next_level_left.animate.scale(
+            all_objects.animate.scale(
                 0.25),  # Scale factor < 1 makes smaller
             run_time=1
         )
         self.wait(0.1)
-        return next_level_left
+        # return next_level_left
+        return {
+            'starting_circle': next_level_left,
+            'all_objects': all_objects,
+        }
 
-    def make_the_first_iteration_and_return_starting_circle(self):
+    def make_the_first_iteration_and_return_starting_circle(self) -> dict:
         left_circle = self.make_filled_circle()
 
         central_circle = Circle(color=WHITE)
         central_circle.next_to(
             left_circle, RIGHT, buff=0.5)  # set the position
-
+        all_object = VGroup()
+        all_object.add(left_circle, central_circle)
         # show the shapes on screen
         self.play(Create(left_circle),)
         self.wait(0.1)
-        return self.make_1_iteration_and_return_starting_circle(left_circle)
+        return self.make_1_iteration_and_return_starting_circle_and_all_prev_objs(left_circle, all_object)
 
     def construct(self):
 
-        st_circ = self.make_the_first_iteration_and_return_starting_circle()
-        self.make_1_iteration_and_return_starting_circle(st_circ)
+        res_dct = self.make_the_first_iteration_and_return_starting_circle()
+        res_dct = self.make_1_iteration_and_return_starting_circle_and_all_prev_objs(
+            res_dct['starting_circle'],
+            res_dct['all_objects'],
+        )
+        res_dct['starting_circle']
+        res_dct['all_objects']
 
     def make_connecting_line(self, obj1, obj2):
         # Calculate points on the circles' boundaries
@@ -86,6 +115,8 @@ class StartingFromTheLeftCircle(Scene):
         end_point = obj2.get_center() - direction * obj2.radius
         print(f'start_point', start_point)
         print(f'end_point', end_point)
+        print(f'obj1.radius', obj1.radius)
+        print(f'obj2.radius', obj2.radius)
         print()
         # print(f'start_point', start_point)
         # Draw the line
