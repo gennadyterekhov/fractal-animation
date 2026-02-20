db240138e7fa0c721ee7d3bef70a7dc1048c976b
Gennady Terekhov <ter.ge@yandex.ru>
2026-02-20 15:30:36 +0300

    upd

--- cmd/circle_with_n_rays.py
+++ cmd/circle_with_n_rays.py
@@ -30,43 +30,18 @@ class CircleWithNRays(Scene):
         )
 
     def make_1_iteration(self, entry_data: EntryData) -> EntryData:
-        print('[make_1_iteration]', entry_data.current_iteration)
-
         central_circle = Circle()
 
         entry_data.all_objects.add(central_circle)
         self.play(Create(central_circle))
-        self.wait(0.1)
 
-        print(f'will have {entry_data.rays} rays')
         anims = []
         for i in range(entry_data.rays):
             print(f'iter {i}')
             (x, y, z) = get_direction(i, entry_data.rays)
-            _cntr_pnt = Dot(point=[0,0,0], color=RED)
             pnt = Dot(point=[x * 2.5, y * 2.5, 0], color=PURPLE)
-            virtual_circle_w_centers = Circle(
-                radius=2.5,
-                color=BLUE,
-                stroke_width=2
-            )
-            # self.add(_cntr_pnt)
-            # self.add(pnt)
-            # self.add(virtual_circle_w_centers)
-            crcl = Circle(
-                radius=1,
-                color=WHITE,
-                stroke_width=2,
-            ).move_to(pnt.get_center())
-            crcl.set_fill(GREEN, opacity=0.8)
             cl = make_connecting_line(pnt, central_circle)
-
-            # entry_data.all_objects.add(virtual_circle_w_centers)
-            # entry_data.all_objects.add(pnt)
-            # entry_data.all_objects.add(crcl)
             entry_data.all_objects.add(cl)
-
-            # anims.append(Create(crcl))
             anims.append(Create(cl))
 
         self.play(*anims)
@@ -75,7 +50,6 @@ class CircleWithNRays(Scene):
             entry_data.all_objects.animate.scale(0.25, about_point=(0,0,0)),
             run_time=1
         )
-        self.wait(0.1)
         res_data = EntryData(
             entry_data.all_objects,
             entry_data.rays,
