b032097bb941d75eb645c7a0996355b62eabf234
Gennady Terekhov <ter.ge@yandex.ru>
2026-02-20 11:02:39 +0300

    upd

--- src/angle.py
+++ src/angle.py
@@ -3,7 +3,6 @@ import math
 
 def get_angle(iter: int, rays: int) -> float:
     one_ray_degree = 360/rays
-    print('one_ray_degree*iter', one_ray_degree*iter)
     angle = 180-one_ray_degree*iter
     return angle
 
